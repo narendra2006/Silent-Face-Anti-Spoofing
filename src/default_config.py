58e1928c87f8a169aa90f4df61fb3487c9f97ad7
NARENDRA WIRA ADITAMA <143769605+narendra2006@users.noreply.github.com>
2026-04-07 11:28:58 +0700

    Update default training configuration parameters

--- src/default_config.py
+++ src/default_config.py
@@ -19,13 +19,13 @@ def get_default_config():
     conf = EasyDict()
 
     # ----------------------training---------------
-    conf.lr = 1e-1
+    conf.lr = 1e-3
     # [9, 13, 15]
-    conf.milestones = [10, 15, 22]  # down learing rate
+    conf.milestones = [50, 100, 130]  # down learing rate
     conf.gamma = 0.1
-    conf.epochs = 25
+    conf.epochs = 150
     conf.momentum = 0.9
-    conf.batch_size = 1024
+    conf.batch_size = 16
 
     # model
     conf.num_classes = 2
