859dd7fa70b02f22445253926aa5589cc5c62656
NARENDRA WIRA ADITAMA <143769605+narendra2006@users.noreply.github.com>
2026-04-08 14:12:25 +0700

    Refactor anti-spoof prediction class and methods

--- src/anti_spoof_predict.py
+++ src/anti_spoof_predict.py
@@ -1,103 +1,52 @@
-# -*- coding: utf-8 -*-
-# @Time : 20-6-9 上午10:20
-# @Author : zhuying
-# @Company : Minivision
-# @File : anti_spoof_predict.py
-# @Software : PyCharm
-
-import os
-import cv2
-import math
-import torch
-import numpy as np
+import os, cv2, torch, re
 import torch.nn.functional as F
+from collections import OrderedDict
+from src.model_lib.MiniFASNet import MiniFASNetV1SE, MiniFASNetV2
+from src.data_io.transform import SDKTestTransform
+from src.utility import parse_model_name
 
+MODEL_DICT = {'MiniFASNetV1SE': MiniFASNetV1SE, 'MiniFASNetV2': MiniFASNetV2}
 
-from src.model_lib.MiniFASNet import MiniFASNetV1, MiniFASNetV2,MiniFASNetV1SE,MiniFASNetV2SE
-from src.data_io import transform as trans
-from src.utility import get_kernel, parse_model_name
-
-MODEL_MAPPING = {
-    'MiniFASNetV1': MiniFASNetV1,
-    'MiniFASNetV2': MiniFASNetV2,
-    'MiniFASNetV1SE':MiniFASNetV1SE,
-    'MiniFASNetV2SE':MiniFASNetV2SE
-}
-
-
-class Detection:
-    def __init__(self):
-        caffemodel = "./resources/detection_model/Widerface-RetinaFace.caffemodel"
-        deploy = "./resources/detection_model/deploy.prototxt"
-        self.detector = cv2.dnn.readNetFromCaffe(deploy, caffemodel)
-        self.detector_confidence = 0.6
-
-    def get_bbox(self, img):
-        height, width = img.shape[0], img.shape[1]
-        aspect_ratio = width / height
-        if img.shape[1] * img.shape[0] >= 192 * 192:
-            img = cv2.resize(img,
-                             (int(192 * math.sqrt(aspect_ratio)),
-                              int(192 / math.sqrt(aspect_ratio))), interpolation=cv2.INTER_LINEAR)
-
-        blob = cv2.dnn.blobFromImage(img, 1, mean=(104, 117, 123))
-        self.detector.setInput(blob, 'data')
-        out = self.detector.forward('detection_out').squeeze()
-        max_conf_index = np.argmax(out[:, 2])
-        left, top, right, bottom = out[max_conf_index, 3]*width, out[max_conf_index, 4]*height, \
-                                   out[max_conf_index, 5]*width, out[max_conf_index, 6]*height
-        bbox = [int(left), int(top), int(right-left+1), int(bottom-top+1)]
-        return bbox
-
-
-class AntiSpoofPredict(Detection):
-    def __init__(self, device_id):
-        super(AntiSpoofPredict, self).__init__()
-        self.device = torch.device("cuda:{}".format(device_id)
-                                   if torch.cuda.is_available() else "cpu")
-
+class AntiSpoofPredict(object):
+    def __init__(self, device_id): 
+        self.device = torch.device(f'cuda:{device_id}' if torch.cuda.is_available() else 'cpu')
+        
     def _load_model(self, model_path):
-        # define model
         model_name = os.path.basename(model_path)
-        h_input, w_input, model_type, _ = parse_model_name(model_name)
-        self.kernel_size = get_kernel(h_input, w_input,)
-        self.model = MODEL_MAPPING[model_type](conv6_kernel=self.kernel_size).to(self.device)
-
-        # load model weight
-        state_dict = torch.load(model_path, map_location=self.device)
-        keys = iter(state_dict)
-        first_layer_name = keys.__next__()
-        if first_layer_name.find('module.') >= 0:
-            from collections import OrderedDict
-            new_state_dict = OrderedDict()
-            for key, value in state_dict.items():
-                name_key = key[7:]
-                new_state_dict[name_key] = value
-            self.model.load_state_dict(new_state_dict)
-        else:
-            self.model.load_state_dict(state_dict)
-        return None
-
-    def predict(self, img, model_path):
-        test_transform = trans.Compose([
-            trans.ToTensor(),
-        ])
-        img = test_transform(img)
-        img = img.unsqueeze(0).to(self.device)
-        self._load_model(model_path)
-        self.model.eval()
-        with torch.no_grad():
-            result = self.model.forward(img)
-            result = F.softmax(result).cpu().numpy()
-        return result
-
-
-
-
-
-
-
-
-
-
-
+        h, w, m_type, _ = parse_model_name(model_name)
+        self.model = MODEL_DICT[m_type](conv6_kernel=(h//16, w//16)).to(self.device)
+        
+        # Load raw state_dict
+        sd = torch.load(model_path, map_location=self.device)
+        clean_sd = OrderedDict()
+        
+        # Proses pembersihan otomatis (Auto-Cleaner v2)
+        for k, v in sd.items():
+            # 1. Buang FTGenerator karena tidak dipakai saat inferensi/prediksi
+            if 'FTGenerator' in k:
+                continue
+                
+            # 2. Bersihkan prefix 'module.' dan 'model.' di awal string
+            new_k = k
+            if new_k.startswith('module.'):
+                new_k = new_k.replace('module.', '', 1)
+            if new_k.startswith('model.'):
+                new_k = new_k.replace('model.', '', 1)
+            
+            # 3. Perbaiki missing layer path jika masih ada (contoh: conv_3.0. -> conv_3.model.0.)
+            new_k = re.sub(r'conv_(\d+)\.(\d+)\.', r'conv_\1.model.\2.', new_k)
+            
+            clean_sd[new_k] = v
+            
+        # Load clean weights ke dalam model
+        self.model.load_state_dict(clean_sd, strict=True)
+        
+    def predict(self, img, path):
+        self._load_model(path); self.model.eval()
+        img = SDKTestTransform()(img).unsqueeze(0).to(self.device)
+        with torch.no_grad(): return F.softmax(self.model(img), dim=-1).cpu().numpy()
+        
+    def get_bbox(self, img):
+        fd = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        fs = fd.detectMultiScale(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 1.1, 4)
+        return list(fs[0]) if len(fs) > 0 else [0, 0, img.shape[1], img.shape[0]]
