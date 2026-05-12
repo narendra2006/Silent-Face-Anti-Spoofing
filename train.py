80592ba6a13d6dd78197c52ef6e73441dd291cd4
NARENDRA WIRA ADITAMA <143769605+narendra2006@users.noreply.github.com>
2026-05-12 09:22:06 +0700

    Remove class weights from CrossEntropyLoss
    Removed class weights from the loss function.

--- train.py
+++ train.py
@@ -105,9 +105,6 @@ if __name__ == "__main__":
     set_seed(42)
     args    = parse_args()
     validasi_dataset(args.patch_info)
-
-    class_weights = torch.FloatTensor([2.0, 1.0]).to(device)
-    criterion     = torch.nn.CrossEntropyLoss(weight=class_weights)
         
     conf    = get_default_config()
     conf    = update_config(args, conf)
